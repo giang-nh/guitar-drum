f2bb5f8d1c55329f984ec3753125e0f5a895290e
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 17:44:18 +0700

    Refresh PWA cache for Auto-Tune suggestions

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v25";
+const CACHE = "guitar-drum-v26";
 const ASSETS = ["./", "./index.html", "./tone.js", "./guitar-follow.js", "./tone-references.json", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
