7a6eda9dcb985261793fce236f3921555ebd384e
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 12:48:56 +0700

    Refresh offline cache for latest UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v1";
+const CACHE = "guitar-drum-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
