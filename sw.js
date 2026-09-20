569f12b235324884ce7a11f96a3a53e63278098b
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 13:15:09 +0700

    Bump cache for third song

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v5";
+const CACHE = "guitar-drum-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
