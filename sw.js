8e318e1bac018f8385d4a875d5939f5f2586f3c7
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 18:15:14 +0700

    Refresh PWA cache for Playing and Developer modes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v30";
+const CACHE = "guitar-drum-v31";
 const ASSETS = ["./", "./index.html", "./tone.js", "./guitar-follow.js", "./tone-references.json", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
