33526e84974fcd23e4f867e4c5d840b3821cd544
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 17:04:36 +0700

    Refresh PWA cache for stop resume intent

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v18";
+const CACHE = "guitar-drum-v19";
 const ASSETS = ["./", "./index.html", "./tone.js", "./guitar-follow.js", "./tone-references.json", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
