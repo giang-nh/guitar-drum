44a25b5c09df0656ec3ba2e1107a5c9b19939a47
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 17:23:39 +0700

    Refresh PWA cache for debug telemetry

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v22";
+const CACHE = "guitar-drum-v23";
 const ASSETS = ["./", "./index.html", "./tone.js", "./guitar-follow.js", "./tone-references.json", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
