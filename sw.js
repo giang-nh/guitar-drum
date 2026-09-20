38b062bdf8d64819173270b7b370704b6240f3d5
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 18:13:29 +0700

    Bump PWA cache for in-app help

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v29";
+const CACHE = "guitar-drum-v30";
 const ASSETS = ["./", "./index.html", "./tone.js", "./guitar-follow.js", "./tone-references.json", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
