cd1cf8e2b7afae6ede4f383d2fc72b7085a6f425
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 13:27:53 +0700

    Bump cache for compact lyrics and tap pause

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v6";
+const CACHE = "guitar-drum-v7";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
