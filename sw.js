9c2b861135bd94b0ee52cef59db7cedd2bca29ab
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 16:45:45 +0700

    Refresh PWA cache for section follow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v14";
+const CACHE = "guitar-drum-v15";
 const ASSETS = ["./", "./index.html", "./tone.js", "./guitar-follow.js", "./tone-references.json", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
