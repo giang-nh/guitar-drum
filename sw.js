3c6fdaa1f01134ae89d6397cc4cd4bea0f596f96
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 13:05:15 +0700

    Bump cache for layout and tone hints

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v3";
+const CACHE = "guitar-drum-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
