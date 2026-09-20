48e16c514fa204c9f76a96d41871d9b728515b3c
giang-nh <nguyenhaugiang@gmail.com>
2026-09-20 13:12:38 +0700

    Bump cache for compact lyric sheet

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guitar-drum-v4";
+const CACHE = "guitar-drum-v5";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", event => {
